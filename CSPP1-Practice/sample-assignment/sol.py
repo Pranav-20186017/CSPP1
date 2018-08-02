ee6465effb825d1a16fc3d71473b612ca32d278c
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-02 13:01:49 +0530

    " -> 0 of 1 passed. pylint: 0/0 "

--- CSPP1-Practice/sample-assignment/sol.py
+++ CSPP1-Practice/sample-assignment/sol.py
@@ -4,9 +4,7 @@ Date: 2-Aug-2018
 Encdoing: UTF-8
 '''
 def main():
-	'''
-	Main Function, calculate sum  
-	'''
+	''' Main Function, calculate sum  '''
     data = input()
     print(int(data[0]) + int(data[3]))
 
