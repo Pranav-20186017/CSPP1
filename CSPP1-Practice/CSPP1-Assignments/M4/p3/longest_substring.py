1eaebddf62faef7af9acfb359f9f06457da22082
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-02 17:37:24 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- CSPP1-Practice/CSPP1-Assignments/M4/p3/longest_substring.py
+++ CSPP1-Practice/CSPP1-Assignments/M4/p3/longest_substring.py
@@ -9,17 +9,17 @@ def main():
     s_t = input()
     max_len = 0
     current = s_t[0]
-    longest = s_t[0]
+    next_point = s_t[0]
     i = 0
     for i in range(len(s_t) - 1):
         if s_t[i + 1] >= s_t[i]:
             current += s_t[i + 1]
             if len(current) > max_len:
                 max_len = len(current)
-                longest = current
+                next_point = current
         else:
             current = s_t[i + 1]
     i += 1
-    print(longest)
+    print(next_point)
 if __name__ == "__main__":
     main()
