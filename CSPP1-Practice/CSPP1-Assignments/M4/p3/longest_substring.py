aa442f643485716da253615b17af298c639761fb
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-02 17:32:18 +0530

    " -> 0 of 5 passed. pylint: 3.75/10 "

--- CSPP1-Practice/CSPP1-Assignments/M4/p3/longest_substring.py
+++ CSPP1-Practice/CSPP1-Assignments/M4/p3/longest_substring.py
@@ -6,19 +6,19 @@ Encding: UTF-8
 def main():
     """This function prints the longest substring that
      follows the alphabetical order in a given string"""
-    s_t = input()
-    m_l = 0
-    cu_st = s_t[0]
-    lo_st = s_t[0]
-    for i in range(len(s_t) - 1):
-        if s_t[i + 1] >= s_t[i]:
-            cu_st += s_t[i + 1]
-            if len(cu_st) > m_l:
-                m_l = len(cu_st)
-                lo_st = cu_st
+    s = input()
+    maxLen=0
+    current=s[0]
+    longest=s[0]
+    for i in range(len(s) - 1):
+        if s[i + 1] >= s[i]:
+            current += s[i + 1]
+            if len(current) > maxLen:
+                maxLen = len(current)
+                longest = current
         else:
-            cu_st = s_t[i + 1]
-        i += 1
-print(lo_st)
+            current=s[i + 1]    
+    i += 1
+    print ('Longest substring in alphabetical order is: ' + longest)
 if __name__ == "__main__":
     main()
