3c74d49aa190a134557b8f71192bebcda2e0da58
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-02 14:44:17 +0530

    " -> 4 of 4 passed. pylint: 5.00/10 "

--- CSPP1-Practice/CSPP1-Assignments/M4/p1/vowels_counter.py
+++ CSPP1-Practice/CSPP1-Assignments/M4/p1/vowels_counter.py
@@ -12,12 +12,12 @@ Encoding: UTF-8
 def main():
     """this function prints the number of vowles in the input string"""
     s = input()
-    v = ['a','e','i','o','u']
+    v = ['a', 'e', 'i', 'o', 'u']
     vc = 0
     for i in s:
         if i in v:
             vc += 1
     print(vc)
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
