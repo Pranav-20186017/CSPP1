d3d5caa56e121fa2bfcaa4aa4c1a83e81a222edb
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-02 16:40:54 +0530

    " -> 0 of 5 passed. pylint: 0/0 "

--- CSPP1-Practice/CSPP1-Assignments/M4/p2/bob_counter.py
+++ CSPP1-Practice/CSPP1-Assignments/M4/p2/bob_counter.py
@@ -4,11 +4,11 @@ Date: 2 August 2018
 Encoding: UTF-8
 '''
 def main():
-	"""This function prints the number of \
-	times the string bob appears in a given input string"""
-	s_t = input()
-	b_c = 0
-	for i in range(0, len(s_t)-1):
+    """This function prints the number of \
+    times the string bob appears in a given input string"""
+    s_t = input()
+    b_c = 0
+    for i in range(0, len(s_t)-1):
 		if s_t[i] == 'b' and s_t[i+1] == 'o' and s_t[i+2] == 'b':
 			b_c += 1
 	print(b_c)
