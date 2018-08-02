9fd5d557f7582d0d55e5e33156e34a3584c206dc
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-02 17:37:50 +0530

    " -> 0 of 5 passed. pylint: 7.06/10 "

--- CSPP1-Practice/CSPP1-Assignments/M4/p3/longest_substring.py
+++ CSPP1-Practice/CSPP1-Assignments/M4/p3/longest_substring.py
@@ -8,17 +8,17 @@ def main():
      follows the alphabetical order in a given string"""
     s_t = input()
     max_len = 0
-    current = s_t[0]
+    current_point = s_t[0]
     next_point = s_t[0]
     i = 0
     for i in range(len(s_t) - 1):
         if s_t[i + 1] >= s_t[i]:
-            current += s_t[i + 1]
+            current_point += s_t[i + 1]
             if len(current) > max_len:
-                max_len = len(current)
-                next_point = current
+                max_len = len(current_point)
+                next_point = current_point
         else:
-            current = s_t[i + 1]
+            current_point = s_t[i + 1]
     i += 1
     print(next_point)
 if __name__ == "__main__":
