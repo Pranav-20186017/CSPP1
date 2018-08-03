f6fe30f0abe3d5a0e40b1c7a3afc6df37ba80446
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-03 11:39:26 +0530

    M5 A8 Commit

--- CSPP1-Practice/CSPP1-Assignments/M5/guess_my_number.py
+++ CSPP1-Practice/CSPP1-Assignments/M5/guess_my_number.py
@@ -3,15 +3,22 @@ Author: Pranav Surampudi
 Date: 3 August 2018
 Encoding: UTF-8
 '''
-MAX = 100
-MIN = 0
-CH = ''
-while CH != 'c':
-    GUESS = (MIN + MAX)// 2
-    print("is your guess "+ str(GUESS))
-    CH = input("Enter your choice: ")
-    if CH == 'l':
-        MIN = GUESS
-    else:
-        MAX = GUESS
-print("The number you guessed is "+ str(GUESS))
+def main():
+    """This function prints the number
+    that the user has guessed """
+    max_a = 100
+    min_a = 0
+    c_h = ''
+    while c_h != 'c':
+        guess_a = (min_a + max_a)// 2
+        print("is your guess "+ str(guess_a))
+        c_h = input("Enter your choice: ")
+        if c_h == 'l':
+            min_a = guess_a
+        else:
+            max_a = guess_a
+    print("The number you guessed is "+ str(guess_a))
+
+
+if __name__ == "__main__":
+    main()
