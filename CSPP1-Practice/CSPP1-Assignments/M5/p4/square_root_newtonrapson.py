77a4512328183cd4ca1c8441f56f03f420c85b5f
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-03 18:59:22 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- CSPP1-Practice/CSPP1-Assignments/M5/p4/square_root_newtonrapson.py
+++ CSPP1-Practice/CSPP1-Assignments/M5/p4/square_root_newtonrapson.py
@@ -9,21 +9,19 @@
 # input: 49
 # output: 6.999999999999991
 '''
-Author: Pranav Surampudi
-Date: 3 August 2018
+Author : Pranav Surampudi
+Date : 03 August 2018
 Encoding: utf-8
+
 '''
 def main():
-    """This function computes the square root
-    of a given number using newton rapson method"""
-    # epsilon and step are initialized
-    # don't change these values
+    ''' This function computes the square root
+     of given number using Newton- Rapson method'''
+    square = int(input())
     epsilon = 0.01
-    inp_number = 81.0
-    guess = inp_number / 2.0
-    while abs(guess * guess - inp_number) >= epsilon:
-        guess = guess - (((guess ** 2) - inp_number)/ (2 * guess))
-
-    print("Square root of "+ str(round(inp_number)) + ' is about '+ str(guess))
+    guess = square / 2.0
+    while abs(guess**2 - square) >= epsilon:
+        guess = guess - (((guess ** 2) - square) / (2 * guess))
+    print(guess)
 if __name__ == "__main__":
     main()
