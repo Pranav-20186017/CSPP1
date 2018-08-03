9b4ac4fc82ac3a208a932e3a388ff346f5648310
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-03 18:07:17 +0530

    " -> 0 of 1 passed. pylint: 10.00/10 "

--- CSPP1-Practice/CSPP1-Assignments/M5/p2/square_root.py
+++ CSPP1-Practice/CSPP1-Assignments/M5/p2/square_root.py
@@ -6,7 +6,7 @@ Encoding: UTF-8
 def main():
     """This function computes the square root
     of a given number using approximation method"""
-    square = 121
+    square = 25
     epsilon = 0.01
     guess = 0.0
     increment = 0.1
@@ -15,6 +15,6 @@ def main():
     if abs(guess**2 - square) >= epsilon:
         print('Failed to find the square root of', square)
     else:
-        print(round(guess))
+        print(guess)
 if __name__ == "__main__":
     main()
