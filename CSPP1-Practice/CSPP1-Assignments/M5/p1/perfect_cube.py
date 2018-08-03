1f1779dab439bef22957d1dcdd7f9349cf5f936f
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-03 14:26:10 +0530

    " -> 3 of 4 passed. pylint: 10.00/10 "

--- CSPP1-Practice/CSPP1-Assignments/M5/p1/perfect_cube.py
+++ CSPP1-Practice/CSPP1-Assignments/M5/p1/perfect_cube.py
@@ -8,7 +8,7 @@ def main():
     a given number is a perfect cube"""
     n_a = int(input())
     t_a = 0
-    for i in range(1, n_a+1, 1):
+    for i in range(1, n_a + 1, 1):
         if i**3 == n_a:
             t_a += 1
             print(str(n_a) + " is a perfect cube")
