7928957b5c8049940fdf97c978d00ae116a1ab44
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-03 14:23:38 +0530

    " -> 0 of 4 passed. pylint: 10.00/10 "

---
+++ CSPP1-Practice/CSPP1-Assignments/M5/p1/perfect_cube.py
@@ -0,0 +1,19 @@
+'''
+Author: Pranav Surampudi
+Date: 3 Aug 2018
+Encoding: UTF-8
+'''
+def main():
+    """This fucntion checks whether or not
+    a given number is a perfect cube"""
+    n_a = input()
+    t_a = 0
+    for i in range(1, n_a+1, 1):
+        if i**3 == n_a:
+            t_a += 1
+            print(str(n_a) + " is a perfect cube")
+            exit()
+    if t_a == 0:
+        print(str(n_a)+" is not a perfect cube")
+if __name__ == "__main__":
+    main()
