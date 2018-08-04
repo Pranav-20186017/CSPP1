b685caff70cc94c81e014adc1a093cde3ad24d8f
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-04 10:15:28 +0530

    " -> 4 of 7 passed. pylint: 10.00/10 "

--- CSPP1-Practice/CSPP1-Assignments/M6/p3/digit_product.py
+++ CSPP1-Practice/CSPP1-Assignments/M6/p3/digit_product.py
@@ -1,15 +1,24 @@
 '''
 Given a  number int_input, find the product of all the digits
-example: 
-	input: 123
-	output: 6
+example:
+    input: 123
+    output: 6
+****************
+Author: Pranav Surampudi
+Date: 04 August 2018
+Encoding: utf-8
 '''
 def main():
     '''
     Read any number from the input, store it in variable int_input.
     '''
     int_input = int(input())
-
-
+    num = int_input
+    product = 1
+    while num != 0:
+        temp = num % 10
+        product *= temp
+        num //= 10
+    print(product)
 if __name__ == "__main__":
     main()
