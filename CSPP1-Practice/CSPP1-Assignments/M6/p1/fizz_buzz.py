6969720f9787601230871916a90e494c82d209be
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-04 10:19:28 +0530

    " -> 7 of 7 passed. pylint: 10.00/10 "

--- CSPP1-Practice/CSPP1-Assignments/M6/p1/fizz_buzz.py
+++ CSPP1-Practice/CSPP1-Assignments/M6/p1/fizz_buzz.py
@@ -15,7 +15,7 @@ def main():
     '''
     num = int(input())
     for i in range(1, num+1, 1):
-        if i % 3 == 0 and i % 5 ==0:
+        if i % 3 == 0 and i % 5 == 0:
             print("Fizz")
             print("Buzz")
         elif i % 5 == 0:
