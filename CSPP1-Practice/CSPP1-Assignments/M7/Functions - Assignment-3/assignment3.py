4a9a48d32794c386a57968da7e3c5099683117cf
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-06 18:42:30 +0530

    " -> 8 of 10 passed. pylint: 9.13/10 "

--- CSPP1-Practice/CSPP1-Assignments/M7/Functions - Assignment-3/assignment3.py
+++ CSPP1-Practice/CSPP1-Assignments/M7/Functions - Assignment-3/assignment3.py
@@ -29,6 +29,6 @@ def main():
     # data = "4773 0.2"
     data = data.split(' ')
     data = list(map(float, data))
-    paying_debt_off_in_a_year(data[0], data[1]) 
+    paying_debt_off_in_a_year(data[0], data[1])
 if __name__ == "__main__":
     main()
