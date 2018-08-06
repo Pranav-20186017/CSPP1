da7523c4e2d416babef5290a9e83b89fd5dcebea
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-06 17:55:45 +0530

    " -> 0 of 10 passed. pylint: 0.50/10 "

--- CSPP1-Practice/CSPP1-Assignments/M7/Functions - Assignment-2/assignment2.py
+++ CSPP1-Practice/CSPP1-Assignments/M7/Functions - Assignment-2/assignment2.py
@@ -32,12 +32,12 @@ def payingDebtOffInAYear(balance, annualInterestRate):
             newbalance -= monthlyPayment
             newbalance += (monthlyInterestRate * newbalance)
             month += 1    
-    return ("Lowest Payment: ",monthlyPayment)
+    print("Lowest Payment: ",monthlyPayment)
 def main():
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print(payingDebtOffInAYear(data[0],data[1]))
+    (payingDebtOffInAYear(data[0],data[1]))
     
 if __name__== "__main__":
     main()
