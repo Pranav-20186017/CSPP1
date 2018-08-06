1e994b7047c30fa15137cf7bdeac540df2735d5b
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-06 16:30:59 +0530

    Module 7 A15 Commit

---
+++ CSPP1-Practice/CSPP1-Assignments/M7/Odd Exercise/odd.py
@@ -0,0 +1,25 @@
+# Exercise: odd
+
+# Write a Python function, odd, that takes in one number and returns True when the number is odd and False otherwise.
+
+# You should use the % (mod) operator, not if.
+
+# This function takes in one number and returns a boolean.
+'''
+*************************
+Author: Pranav Surampudi
+Date: 6 August 2018
+Encoding: utf-8
+*************************
+'''
+def odd(number):
+    """Check for the parity of a given number"""
+    return number % 2 != 0
+    
+
+def main():
+    data = input()
+    print(odd(int(data)))
+
+if __name__== "__main__":
+    main()
