d0a38fcae6055ebd5ffee1e2fb8753b6d3b02f8f
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-06 18:01:21 +0530

    " -> 10 of 10 passed. pylint: 6.00/10 "

--- CSPP1-Practice/CSPP1-Assignments/M7/Functions - Assignment-2/assignment2.py
+++ CSPP1-Practice/CSPP1-Assignments/M7/Functions - Assignment-2/assignment2.py
@@ -47,7 +47,6 @@ def main():
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    (payingDebtOffInAYear(data[0], data[1]))
-    
-if __name__== "__main__":
+    (payingDebtOffInAYear(data[0], data[1])) 
+if __name__ == "__main__":
     main()
