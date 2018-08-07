7c6e1f4fb0467ba6e806897cbc1ac232bd583eb8
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-07 16:25:05 +0530

    " -> 10 of 10 passed. pylint: 8.89/10 "

--- CSPP1-Practice/CSPP1-Assignments/M8/p2/assignment2.py
+++ CSPP1-Practice/CSPP1-Assignments/M8/p2/assignment2.py
@@ -1,18 +1,18 @@
 '''
-************************************
+**************************************
 Author: Pranav Surampudi
 Date: 7 August 2018
 Encoding: Utf-8
-***********************************
+**************************************
 '''
 def sumofdigits(number):
-    """Compute the sum of digits in a number"""
+    """Find Sum of digits"""
     if number == 0:
         return 0
     return number % 10 + sumofdigits(number // 10)
 def main():
-    """Inside Main Function"""
+    """Main Function"""
     a_var = input()
-    print(sumofdigits(int(a_var))
+    print(sumofdigits(int(a_var)))  
 if __name__ == "__main__":
     main()
