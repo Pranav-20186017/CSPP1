16016fc22df74f23f5d1cab3e831a8235dbad1b6
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-07 14:30:30 +0530

    M8 A3 Commit

---
+++ CSPP1-Practice/CSPP1-Assignments/M8/power using recursion/power_recr.py
@@ -0,0 +1,20 @@
+"""
+*************************
+Auhtor: Pranav Surampudi
+Date: 7 August 2018
+Encoding: Utf-8
+************************
+"""
+def recur_power(base, exp):
+    """Compute expoent using recursion"""
+    if exp == 1:
+        return base * 1
+    if exp != 1:
+        return base * recur_power(base, exp-1)
+def main():
+    """Main Function"""
+    data = input()
+    data = data.split()
+    print(recur_power(float(data[0]), int(data[1])))
+if __name__ == "__main__":
+    main()
