c0486c11c41be2d75f691fdef077314db71f3294
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-08 13:55:56 +0530

    " -> 0 of 9 passed. pylint: 0/0 "

---
+++ CSPP1-Practice/CSPP1-Assignments/M9/p1/assignment1.py
@@ -0,0 +1,37 @@
+'''
+Exercise: Assignment-1
+First, implement the function isWordGuessed that takes in two parameters -
+a string, secret_word, and a list of letters, letters_guessed. This function
+returns a boolean - True if secret_word has been guessed (ie, all the letters of
+secret_word are in letters_guessed) and False otherwise.
+'''
+
+
+def is_word_guessed(secret_word, letters_guessed):
+   temp_list = list(secret_word)
+   count = 0
+   for i in temp_list:
+       if i in letters_guessed:
+           count += 1
+    if count == len(secret_word):
+        return True
+    else:
+        return False
+def main():
+    '''
+    Main function for the program
+    '''
+    user_input = input()
+    if user_input:
+        data = user_input.split()
+        secret_word = data[0]
+    else:
+        data = []
+        secret_word = ""
+    list1 = []
+    for j in range(1, len(data)):
+        list1.append(data[j][0])
+    print(is_word_guessed(secret_word, list1))
+
+if __name__ == "__main__":
+    main()
