e185c73bd6a8790a4fd6d07433329157173fd7b7
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-10 21:50:13 +0530

    " -> 5 of 5 passed. pylint: 7.86/10 "

--- CSPP1-Practice/CSPP1-Assignments/M11/p4/assignment4.py
+++ CSPP1-Practice/CSPP1-Assignments/M11/p4/assignment4.py
@@ -5,7 +5,7 @@ Date: 10 Aug 2018
 Encoding: utf-8
 *************************
 '''
-def calculateHandlen(hand):
+def calculate_hand_len(hand):
     """compute the alphabets in a hand"""
     temp_list = list(hand.values())
     ans = sum(temp_list)
@@ -18,6 +18,6 @@ def main():
         data = input()
         l = data.split()
         adict[l[0]] = int(l[1])
-    print(calculateHandlen(adict))
+    print(calculate_hand_len(adict))
 if __name__ == "__main__":
     main()
