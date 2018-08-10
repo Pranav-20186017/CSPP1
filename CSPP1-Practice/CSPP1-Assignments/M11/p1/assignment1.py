7086425d176b6a932c27fc6f6f0a622e4b76aab2
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-10 14:52:53 +0530

    " -> 9 of 9 passed. pylint: 7.06/10 "

--- CSPP1-Practice/CSPP1-Assignments/M11/p1/assignment1.py
+++ CSPP1-Practice/CSPP1-Assignments/M11/p1/assignment1.py
@@ -8,7 +8,7 @@ Encoding: UTF-8
 
 def get_word_score(word, n):
     """Compute the score for a given word"""
-    word = word.lowercase()
+    word = word.lower()
     SCRABBLE_LETTER_VALUES = {
     'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
     'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
