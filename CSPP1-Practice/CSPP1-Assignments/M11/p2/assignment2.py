7cf17bb237e07df70256c2545a9c9d080a8df82e
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-10 16:40:01 +0530

    " -> 0 of 6 passed. pylint: 6.25/10 "

--- CSPP1-Practice/CSPP1-Assignments/M11/p2/assignment2.py
+++ CSPP1-Practice/CSPP1-Assignments/M11/p2/assignment2.py
@@ -5,21 +5,21 @@ Date: 10 Aug 2018
 Encoding: Utf-8
 **************************
 '''
-def updateHand(hand, word):
+def update_hand(hand, word):
     """Returns an updated hand """
     for i in word:
         if i in list(hand.keys()):
             hand[i] -= 1
-    return hand 
+    return hand
 def main():
     """Main Function"""
-    n = input()
+    number = input()
     adict = {}
-    for i in range(int(n)):
+    for i in range(int(number)):
         data = input()
-        l = data.split()
-        adict[l[0]] = int(l[1])
+        lima = data.split()
+        adict[l[0]] = int(lima[1])
     data1 = input()
-    print(updateHand(adict, data1))
+    print(update_hand(adict, data1))
 if __name__ == "__main__":
     main()
