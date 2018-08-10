2795a61fd2f2fe9df94a330b8285b5a8c4c7dad5
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-10 21:49:52 +0530

    " -> 5 of 5 passed. pylint: 7.14/10 "

--- CSPP1-Practice/CSPP1-Assignments/M11/p4/assignment4.py
+++ CSPP1-Practice/CSPP1-Assignments/M11/p4/assignment4.py
@@ -11,13 +11,13 @@ def calculateHandlen(hand):
     ans = sum(temp_list)
     return ans
 def main():
-	"""Main Function"""
-	n=input()
-	adict={}
-	for i in range(int(n)):
-		data=input()
-		l=data.split()
-		adict[l[0]]=int(l[1])
-	print(calculateHandlen(adict))
+    """Main Function"""
+    n = input()
+    adict = {}
+    for i in range(int(n)):
+        data = input()
+        l = data.split()
+        adict[l[0]] = int(l[1])
+    print(calculateHandlen(adict))
 if __name__ == "__main__":
-	main()
+    main()
