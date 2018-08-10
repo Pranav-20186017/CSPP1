aa6d66efa76c96d8f13e550cf41592ffb75f48cd
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-10 14:54:19 +0530

    " -> 9 of 9 passed. pylint: 7.65/10 "

--- CSPP1-Practice/CSPP1-Assignments/M11/p1/assignment1.py
+++ CSPP1-Practice/CSPP1-Assignments/M11/p1/assignment1.py
@@ -9,7 +9,7 @@ Encoding: UTF-8
 def get_word_score(word, n):
     """Compute the score for a given word"""
     word = word.lower()
-    SCRABBLE_LETTER_VALUES = {
+    scrabble_letter_values = {
     'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
     'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
     's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
@@ -17,7 +17,7 @@ def get_word_score(word, n):
     score = 0
     bonus = 50
     for i in word:
-        score += SCRABBLE_LETTER_VALUES[i]
+        score += scrabble_letter_values[i]
     score *= len(word)
     if len(word) == n:
         score += bonus
