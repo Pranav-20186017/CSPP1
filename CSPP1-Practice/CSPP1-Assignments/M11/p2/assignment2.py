249a68a075f733b7384bb8273cf62ff5b2f10f93
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-10 16:36:05 +0530

    " -> 6 of 6 passed. pylint: -7.50/10 "

--- CSPP1-Practice/CSPP1-Assignments/M11/p2/assignment2.py
+++ CSPP1-Practice/CSPP1-Assignments/M11/p2/assignment2.py
@@ -26,15 +26,12 @@ def updateHand(hand, word):
 
 def main():
 	n=input()
-	adict={}
+	adict = {}
 	for i in range(int(n)):
 		data=input()
 		l=data.split()
 		adict[l[0]]=int(l[1])
 	data1=input()
 	print(updateHand(adict,data1))
-		
-
-
-if __name__== "__main__":
+if __name__ == "__main__":
 	main()
