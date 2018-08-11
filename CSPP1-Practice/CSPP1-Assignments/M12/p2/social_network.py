2f614cd477b00cb1613efddf135dd0770519b426
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-11 11:49:59 +0530

    " -> 0 of 2 passed. pylint: 7.59/10 "

--- CSPP1-Practice/CSPP1-Assignments/M12/p2/social_network.py
+++ CSPP1-Practice/CSPP1-Assignments/M12/p2/social_network.py
@@ -15,7 +15,7 @@ def follow(network, arg1, arg2):
     '''
     
     # remove the pass below and start writing your code
-    network[arg1].append(arg2)
+    network[arg1] += (arg2)
     return network
 
 def unfollow(network, arg1, arg2):
