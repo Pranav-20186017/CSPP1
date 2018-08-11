490108f2fba4325bd2ff0bb9acb09222b96b9df1
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-11 10:28:18 +0530

    " -> 1 of 3 passed. pylint: 5.20/10 "

--- CSPP1-Practice/CSPP1-Assignments/M12/p1/create_social_network.py
+++ CSPP1-Practice/CSPP1-Assignments/M12/p1/create_social_network.py
@@ -43,7 +43,7 @@ def create_social_network(data):
     if 'follows' not in data:
         return adict
     for i in data:
-        l = data.split(" follows ")
+        l = data.split("\n")
         temp_key.append(l[0])
         temp_vals.append(l[1])
     print(temp_key)
