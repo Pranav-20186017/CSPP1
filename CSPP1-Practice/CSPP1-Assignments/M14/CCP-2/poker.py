10b2b1c6e7b1bb3118af4e36fc22f803ac9e1b2f
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-16 21:02:49 +0530

    " -> 19 of 19 passed. pylint: 5.83/10 "

--- CSPP1-Practice/CSPP1-Assignments/M14/CCP-2/poker.py
+++ CSPP1-Practice/CSPP1-Assignments/M14/CCP-2/poker.py
@@ -1,55 +1,68 @@
-'''
-    Author: Pranav Surampudi
-    date : 16-08-2018
-'''
 def hand_values(hand):
-    return sorted(set(["--23456789TJQKA".index(c) for c,x in hand]),reverse=True)
+    ''' Returns the hand values based on the index '''
+    return sorted((["--23456789TJQKA".index(f) for f,s in hand]), reverse = True)
+
 def is_straight(ranks):
+    ''' Check if straight or not ''' 
+    #print(ranks)
     return (max(ranks) - min(ranks) == 4 and len(set(ranks)) == 5) or (ranks[1:5] == [5,4,3,2] and ranks[0] == 14 ) 
+
 def is_flush(hand):
-    value_set = []
+    ''' Check if it is flush '''
+    values_set = []
     for i in hand:
-        value_set.append(i[1])
-    return len(value_set) == 1
-def kind(ranks, n):
+        values_set.append(i[1])
+    return len(set(values_set)) == 1
+
+def which_kind(ranks,n):
+    ''' Returning the rank and checking of the repetition and sending the rank''' 
     for i in ranks:
         if ranks.count(i) == n:
-            return i
+           return i
 def is_two_pair(ranks):
-    high_rep = kind(ranks,2)
-    low_rep = kind(sorted(ranks), 2)
-    if high_rep != low_rep:
-        return high_rep, low_rep, ranks
+    '''Checking if two pair or not '''
+    high_val = which_kind(ranks, 2)
+    low_val = which_kind(sorted(ranks), 2)
+    if high_val != low_val:
+        return high_val, low_val, ranks
+
 def hand_rank(hand):
+    ''' Function to call all the functions '''
     rank = hand_values(hand)
-    if is_straight(rank) and is_flush(hand):
+    if is_straight(rank) and is_flush(hand):#Straightflush
         return 8, rank
-    if kind(rank, 4):
-        return 7, kind(rank, 4), rank
-    if kind(rank,3) and kind(rank, 2):
-        return 6, kind(rank, 3), kind(rank,2), rank
-    if is_flush(hand):
+    if which_kind(rank,4):#Four of a kind
+        return 7, which_kind(rank, 4), rank
+    if which_kind(rank, 3) and which_kind(rank, 2):#Full house
+        return 6, which_kind(rank, 3), which_kind(rank, 2), rank
+    if is_flush(hand):#Flush
         return 5, rank
-    if is_straight(rank):
+    if is_straight(rank):#Straight
         return 4, rank
-    if kind(rank, 3):
-        return 3, kind(rank, 3), rank
-    if is_two_pair(hand):
+    if which_kind(rank, 3):# Three of a kind
+        return 3, which_kind(rank, 3), rank
+    if is_two_pair(rank): #Two pair
         return 2, is_two_pair(rank)
-    if kind(rank, 2):
-        return 1, kind(rank), rank
+    if which_kind(rank, 2): #One pair
+        return 1, which_kind(rank, 2), rank
+  
     return 0, rank
+
 def poker(hands):
+    '''
+    This function is to return the max code
+    '''
     return max(hands, key=hand_rank)
 
 if __name__ == "__main__":
     # read the number of test cases
     COUNT = int(input())
     # iterate through the test cases to set up hands list
+    list_1 = []
     HANDS = []
     for x in range(COUNT):
         line = input()
         ha = line.split(" ")
         HANDS.append(ha)
-    # test the poker function to see how it works
+
     print(' '.join(poker(HANDS)))
