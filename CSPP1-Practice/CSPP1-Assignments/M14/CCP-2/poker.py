6188e0ce8e1f16bdd38813fc5453c0c2a1308d4e
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-16 21:04:48 +0530

    " -> 19 of 19 passed. pylint: 5.83/10 "

--- CSPP1-Practice/CSPP1-Assignments/M14/CCP-2/poker.py
+++ CSPP1-Practice/CSPP1-Assignments/M14/CCP-2/poker.py
@@ -14,15 +14,15 @@ def is_flush(hand):
         values_set.append(i[1])
     return len(set(values_set)) == 1
 
-def which_kind(ranks,n):
+def kind(ranks,n):
     ''' Returning the rank and checking of the repetition and sending the rank''' 
     for i in ranks:
         if ranks.count(i) == n:
            return i
 def is_two_pair(ranks):
     '''Checking if two pair or not '''
-    high_val = which_kind(ranks, 2)
-    low_val = which_kind(sorted(ranks), 2)
+    high_val = kind(ranks, 2)
+    low_val = kind(sorted(ranks), 2)
     if high_val != low_val:
         return high_val, low_val, ranks
 
@@ -31,20 +31,20 @@ def hand_rank(hand):
     rank = hand_values(hand)
     if is_straight(rank) and is_flush(hand):#Straightflush
         return 8, rank
-    if which_kind(rank,4):#Four of a kind
-        return 7, which_kind(rank, 4), rank
-    if which_kind(rank, 3) and which_kind(rank, 2):#Full house
-        return 6, which_kind(rank, 3), which_kind(rank, 2), rank
+    if kind(rank,4):#Four of a kind
+        return 7, kind(rank, 4), rank
+    if kind(rank, 3) and kind(rank, 2):#Full house
+        return 6, kind(rank, 3), kind(rank, 2), rank
     if is_flush(hand):#Flush
         return 5, rank
     if is_straight(rank):#Straight
         return 4, rank
-    if which_kind(rank, 3):# Three of a kind
-        return 3, which_kind(rank, 3), rank
+    if kind(rank, 3):# Three of a kind
+        return 3, kind(rank, 3), rank
     if is_two_pair(rank): #Two pair
         return 2, is_two_pair(rank)
-    if which_kind(rank, 2): #One pair
-        return 1, which_kind(rank, 2), rank
+    if kind(rank, 2): #One pair
+        return 1, kind(rank, 2), rank
   
     return 0, rank
 
