0833e5f239c19903441951ed06236a0d4191ce96
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-17 10:51:59 +0530

    " -> 0 of 3 passed. pylint: 5.41/10 "

--- CSPP1-Practice/CSPP1-Assignments/M16/CodeCampDocumentDistance/document_distance.py
+++ CSPP1-Practice/CSPP1-Assignments/M16/CodeCampDocumentDistance/document_distance.py
@@ -4,22 +4,26 @@
 def generate_list(input1):
     '''generate a dictionary of words'''
     new_input_1 = str(input1.lower())
-    spec_char = "!@#$%^&*(),-_+=;:.?\n"
+    spec_char = "!@#$'%^&*(),-_+=;:.?\n"
     ver_2_inp1 = ""
     for i in new_input_1:
         if i not in spec_char:
             ver_2_inp1 += i
     temp1 = ver_2_inp1.split()
     return temp1
-# def remove_stopwords(l1, l2):
-#     stopwords = load_stopwords("stopwords.txt")
-#     #print(stopwords)
-#     removal= list(stopwords.keys())
-#     #print(removal)
-#     for i in l1:
-#     	if i in removal:
-#     		i=None
-#     print(l1)
+def remove_stopwords(l1, l2):
+    stopwords = load_stopwords("stopwords.txt")
+    #print(stopwords)
+    removal= list(stopwords.keys())
+    #print(removal)
+    for i in range(0, len(l1)+1):
+    	if l1[i] in removal:
+    		del l1[i]
+    for i in range(0, len(l2)+1):
+    	if l2[i] in removal:
+    		del l2[i]
+    print(l1)
+    print(l2)
 # def similarity(dict1, dict2):
 #     '''
 #         Compute the document distance as given in the PDF
@@ -43,11 +47,9 @@ def main():
 
     l1 = generate_list(input1)
     l2= generate_list(input2)
-    #generate_dic(l1, l2)
-    # print(l1)
-    # print(l2)
-    #remove_stopwords(l1, l2)
-    removal = load_stopwords("stopwords.txt")
-    print(list(removal.keys()))
+    generate_dic(l1, l2)
+    print(l1)
+    print(l2)
+    remove_stopwords(l1, l2)
 if __name__ == '__main__':
     main()
