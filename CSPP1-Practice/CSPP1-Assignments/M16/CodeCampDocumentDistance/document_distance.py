787897236a365fdbd603214a27a03b2d9d244736
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-17 15:00:31 +0530

    " -> 3 of 3 passed. pylint: 5.91/10 "

--- CSPP1-Practice/CSPP1-Assignments/M16/CodeCampDocumentDistance/document_distance.py
+++ CSPP1-Practice/CSPP1-Assignments/M16/CodeCampDocumentDistance/document_distance.py
@@ -1,30 +1,54 @@
 '''
     Document Distance - A detailed description is given in the PDF
 '''
-def generate_list(input1):
-    '''generate a dictionary of words'''
-    new_input_1 = str(input1.lower())
-    spec_char = "!@#$'%^&*(),-_+=;:.?\n01235456789"
-    ver_2_inp1 = ""
-    for i in new_input_1:
-        if i not in spec_char:
-            ver_2_inp1 += i
-    temp1 = ver_2_inp1.split()
-    return temp1
-def remove_stopwords(l1, l2):
+import re
+import math
+def tokenize(d1):
+    # ["".join(word) for word in d1 if word.isalpha()]
+    # for word in d1:
+    #     if word.isalpha() or word == " ":
+    #         input_str += word
+    # words_list = d1.lower().split()
+    # for word in words_list:         
+    #     word.strip()
+    word_list1 = []
+    words_list = d1.lower().split()
+    for word in words_list:
+        word_list1.append(re.sub('[^a-z]',"",word).strip())
+    return word_list1
+def build_dict(dictionary, words_list, index):
     stopwords = load_stopwords("stopwords.txt")
-    #print(stopwords)
-    removal= list(stopwords.keys())
-    #print(removal)
-    for i in removal:
-    	if i in l1:
-    		l1.remove(i)
-    print(l1)
-# def similarity(dict1, dict2):
-#     '''
-#         Compute the document distance as given in the PDF
-#     '''
-#      pass
+    for word in words_list:
+        if word not in stopwords and len(word)>0:
+            if word not in dictionary:
+                dictionary[word] = [0, 0]
+            dictionary[word][index] += 1
+    return dictionary
+
+def calculate_distance(dictionary):
+    num = 0
+    den1 = 0
+    den2 = 0
+    for a,b in dictionary.values():
+        num += a * b
+        den1 += a ** 2
+        den2 += b ** 2
+    return num/(math.sqrt(den1)*math.sqrt(den2))
+
+
+
+def similarity(d1, d2):
+    '''
+        Compute the document distance as given in the PDF
+    '''
+    common_dict = dict()
+    string1_words = tokenize(d1)
+    string2_words = tokenize(d2)
+    common_dict = build_dict(common_dict, string1_words,0)
+    common_dict = build_dict(common_dict, string2_words,1)
+    return calculate_distance(common_dict)
+    
+
 def load_stopwords(filename):
     '''
         loads stop words from a file and returns a dictionary
@@ -34,6 +58,7 @@ def load_stopwords(filename):
         for line in filename:
             stopwords[line.strip()] = 0
     return stopwords
+
 def main():
     '''
         take two inputs and call the similarity function
@@ -41,11 +66,7 @@ def main():
     input1 = input()
     input2 = input()
 
-    l1 = generate_list(input1)
-    l2= generate_list(input2)
-    #generate_dic(l1, l2)
-    #print(l1)
-    #print(l2)
-    remove_stopwords(l1, l2)
+    print(similarity(input1, input2))
+
 if __name__ == '__main__':
     main()
