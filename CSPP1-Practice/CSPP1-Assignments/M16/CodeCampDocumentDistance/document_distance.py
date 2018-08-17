e990fc5060b485575709fd3cc3955afbf3d36254
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-17 10:02:45 +0530

    " -> 0 of 3 passed. pylint: 3.75/10 "

--- CSPP1-Practice/CSPP1-Assignments/M16/CodeCampDocumentDistance/document_distance.py
+++ CSPP1-Practice/CSPP1-Assignments/M16/CodeCampDocumentDistance/document_distance.py
@@ -4,23 +4,13 @@
 def generate_dic(input1, input2):
     '''generate a dictionary of words'''
     new_input_1 = str(input1.lower())
-    new_input_2 = str(input2.lower())
     spec_char = "!@#$%^&*(),-_+=;:'.?"
     ver_2_inp1 = ""
-    ver_2_inp2 = ""
     for i in new_input_1:
         if i not in spec_char:
             ver_2_inp1 += i
-    for i in new_input_2:
-        if i not in spec_char:
-            ver_2_inp2 += i
-    # print(ver_2_inp1)
-    # print(ver_2_inp2)
     temp1 = ver_2_inp1.split()
-    temp2 = ver_2_inp2.split()
-    print(temp1)
-    print(temp2)
-
+    return temp1
 # def similarity(dict1, dict2):
 #     '''
 #         Compute the document distance as given in the PDF
@@ -42,7 +32,10 @@ def main():
     input1 = input()
     input2 = input()
 
-    print(generate_dic(input1, input2))
+    l1 = generate_dic(input1)
+    l2= generate_dic(input2)
+    print(l1)
+    print(l2)
 
 if __name__ == '__main__':
     main()
