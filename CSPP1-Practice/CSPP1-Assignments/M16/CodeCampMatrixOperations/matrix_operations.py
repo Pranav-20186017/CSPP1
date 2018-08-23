0c6d5e93bc787a387a65e09a68d9586b713066bd
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-23 12:29:09 +0530

    " -> 8 of 8 passed. pylint: 9.79/10 "

--- CSPP1-Practice/CSPP1-Assignments/M16/CodeCampMatrixOperations/matrix_operations.py
+++ CSPP1-Practice/CSPP1-Assignments/M16/CodeCampMatrixOperations/matrix_operations.py
@@ -15,7 +15,8 @@ def mult_matrix(x_value, y_value, actual_dimension_one, actual_dimension_two):
         error message should be "Error: Matrix shapes invalid for mult"
     '''
     if actual_dimension_one[1] == actual_dimension_two[0]:
-        result = [[sum(a*b for a, b in zip(x_value_row, y_value_col)) for y_value_col in zip(*y_value)] for x_value_row in x_value]
+        result = [[sum(a*b for a, b in zip(x_value_row, y_value_col)) for y_value_col in zip(*y_value)] \
+        for x_value_row in x_value]
         return result
     print("Error: Matrix shapes invalid for mult")
     return None
