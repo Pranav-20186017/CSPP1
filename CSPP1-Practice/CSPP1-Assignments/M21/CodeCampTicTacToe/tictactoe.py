1cd59c6c83c2c77026c4bdbd0f4011708913c7b6
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-24 17:19:02 +0530

    " -> 13 of 14 passed. pylint: -3.04/10 "

--- CSPP1-Practice/CSPP1-Assignments/M21/CodeCampTicTacToe/tictactoe.py
+++ CSPP1-Practice/CSPP1-Assignments/M21/CodeCampTicTacToe/tictactoe.py
@@ -5,12 +5,12 @@ def isvalidinput(board):
 	for i in board:
 		x_sum += i.count('x')
 		o_sum += i.count('o')
-		sum	+= i.count('o') + i.count('x') + i.count(".")	
-	if(x_sum - o_sum not in (0, 1, -1)):
-		print("invalid game")
-		return
+		sum	+= i.count('o') + i.count('x') + i.count(".")
 	if sum != 9:
 		print("invalid input")
+		return	
+	if(x_sum - o_sum not in (0, 1, -1)):
+		print("invalid game")
 		return
 	return True
 		
