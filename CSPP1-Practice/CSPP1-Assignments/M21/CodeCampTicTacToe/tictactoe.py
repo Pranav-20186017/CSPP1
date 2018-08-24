3c04b000725a6b8e15722a8a0c69b7ffbeaf8a21
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-24 17:45:07 +0530

    " -> 5 of 14 passed. pylint: -3.49/10 "

--- CSPP1-Practice/CSPP1-Assignments/M21/CodeCampTicTacToe/tictactoe.py
+++ CSPP1-Practice/CSPP1-Assignments/M21/CodeCampTicTacToe/tictactoe.py
@@ -13,7 +13,22 @@ def isvalidinput(board):
 		print("invalid game")
 		return
 	return True
-		
+
+def checkgame(board):
+	count = 0
+	for i in range(len(board)):
+		if board[i][0] == board[i][1] and board[i][1] == board[i][2]:
+			count += 1
+	for i in range(len(board)):
+		if board[0][i] == board[1][i] and board[1][i] == board[2][i]:
+			count += 1 
+	if board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+		count+= 1
+	if board[0][2] == board[1][1] and board[1][1] == board[2][0]:
+		count += 1
+	if count > 1:
+		print("invalid game")
+		return 
 
 def checkhorizantal(board):
 	for i in range(len(board)):
@@ -54,7 +69,7 @@ def main():
 	board = []
 	for i in range(3):
 		board.append(input().split())
-	if isvalidinput(board):
+	if isvalidinput(board) and checkgame(board):
 		print(checkwinner(board))
 
 main()
