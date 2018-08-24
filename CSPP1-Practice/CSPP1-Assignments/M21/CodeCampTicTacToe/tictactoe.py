005a00a1a484c6bc6f3231bf1ec9a1de323cbb0e
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-24 20:02:45 +0530

    " -> 14 of 14 passed. pylint: 6.98/10 "

--- CSPP1-Practice/CSPP1-Assignments/M21/CodeCampTicTacToe/tictactoe.py
+++ CSPP1-Practice/CSPP1-Assignments/M21/CodeCampTicTacToe/tictactoe.py
@@ -6,77 +6,77 @@ Encoding: utf-8
 ************************************************
 '''
 def isvalidinput(board):
-	'''check for validity of input'''
-	x_sum = 0
-	o_sum = 0
-	sum = 0
-	for i in board:
-		x_sum += i.count('x')
-		o_sum += i.count('o')
-		sum	+= i.count('o') + i.count('x') + i.count(".")
-	if sum != 9:
-		print("invalid input")
-		return	
-	if(x_sum - o_sum not in (0, 1, -1) ):
-		print("invalid game")
-		return
-	return True
+    '''check for validity of input'''
+    x_sum = 0
+    o_sum = 0
+    sum = 0
+    for i in board:
+        x_sum += i.count('x')
+        o_sum += i.count('o')
+        sum += i.count('o') + i.count('x') + i.count(".")
+    if sum != 9:
+        print("invalid input")
+        return  
+    if(x_sum - o_sum not in (0, 1, -1) ):
+        print("invalid game")
+        return
+    return True
 
 def checkgame(board):
-	'''evaluate the game'''
-	count = 0
-	for i in range(len(board)):
-		if board[i][0] == board[i][1] and board[i][1] == board[i][2]:
-			count += 1
-	for i in range(len(board)):
-		if board[0][i] == board[1][i] and board[1][i] == board[2][i]:
-			count += 1 
-	if board[0][0] == board[1][1] and board[1][1] == board[2][2]:
-		count+= 1
-	if board[0][2] == board[1][1] and board[1][1] == board[2][0]:
-		count += 1
-	if count > 1:
-		print("invalid game")
-	else:
-		return True
+    '''evaluate the game'''
+    count = 0
+    for i in range(len(board)):
+        if board[i][0] == board[i][1] and board[i][1] == board[i][2]:
+            count += 1
+    for i in range(len(board)):
+        if board[0][i] == board[1][i] and board[1][i] == board[2][i]:
+            count += 1 
+    if board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+        count+= 1
+    if board[0][2] == board[1][1] and board[1][1] == board[2][0]:
+        count += 1
+    if count > 1:
+        print("invalid game")
+    else:
+        return True
 def checkhorizantal(board):
-	'''check horizontal rows'''
-	for i in range(len(board)):
-		if board[i][0] == board[i][1] and board[i][1] == board[i][2]:
-			return board[i][0]
+    '''check horizontal rows'''
+    for i in range(len(board)):
+        if board[i][0] == board[i][1] and board[i][1] == board[i][2]:
+            return board[i][0]
 def checkvertical(board):
-	'''check vertical rows'''
-	for i in range(len(board)):
-		if board[0][i] == board[1][i] and board[1][i] == board[2][i]:
-			return board[0][i]
+    '''check vertical rows'''
+    for i in range(len(board)):
+        if board[0][i] == board[1][i] and board[1][i] == board[2][i]:
+            return board[0][i]
 def checkdiagonal(board):
-	'''check diagonals'''
-	if board[0][0] == board[1][1] and board[1][1] == board[2][2]:
-		return board[0][0]
-	if board[0][2] == board[1][1] and board[1][1] == board[2][0]:
-		return board[0][2]
+    '''check diagonals'''
+    if board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+        return board[0][0]
+    if board[0][2] == board[1][1] and board[1][1] == board[2][0]:
+        return board[0][2]
 def checkwinner(board):
-	'''compute the winner'''
-	winner =  checkhorizantal(board)
-	winner1 = checkvertical(board)
-	winner2 = checkdiagonal(board)
+    '''compute the winner'''
+    winner =  checkhorizantal(board)
+    winner1 = checkvertical(board)
+    winner2 = checkdiagonal(board)
 
-	if (winner and winner1) or (winner1 and winner2) or (winner and winner2):
-		return ("invalid game")
-		
-	if winner:
-		return winner
-	if winner1:
-		return winner1
-	if winner2:
-		return winner2
-	else:
-		return "draw"
+    if (winner and winner1) or (winner1 and winner2) or (winner and winner2):
+        return ("invalid game")
+        
+    if winner:
+        return winner
+    if winner1:
+        return winner1
+    if winner2:
+        return winner2
+    else:
+        return "draw"
 def main():
-	'''main method'''
-	board = []
-	for i in range(3):
-		board.append(input().split())
-	if isvalidinput(board) and checkgame(board):
-		print(checkwinner(board))
+    '''main method'''
+    board = []
+    for i in range(3):
+        board.append(input().split())
+    if isvalidinput(board) and checkgame(board):
+        print(checkwinner(board))
 main()
