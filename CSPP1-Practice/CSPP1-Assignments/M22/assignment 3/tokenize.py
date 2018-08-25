9423bd8cce860282c3e2b8086c378df54f56ea4f
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-25 11:25:43 +0530

    " -> 0 of 4 passed. pylint: 5.91/10 "

--- CSPP1-Practice/CSPP1-Assignments/M22/assignment 3/tokenize.py
+++ CSPP1-Practice/CSPP1-Assignments/M22/assignment 3/tokenize.py
@@ -2,24 +2,26 @@
 Write a function to tokenize a given string and return a dictionary with the frequency of
 each word
 '''
-def tokenize(temp):
-    new_list=[]
-    for i in range(len(temp)):
-        string = str(temp[i])
-        string = string.strip('",.;')
-        new_list.append(string)
-    dictionary = dict()
-    for i in new_list:
-        if i in dictionary.keys():
-            dictionary[i] += 1
-        else:
-            dictionary[i] = 1
-    return dictionary
+def tokenize(temp,i):
+    if i == 1:
+        new_list=[]
+        for i in range(len(temp)):
+            string = str(temp[i])
+            string = string.strip('",.;')
+            new_list.append(string)
+        print(new_list)
+        dictionary = dict()
+        for i in new_list:
+            if i in dictionary.keys():
+                dictionary[i] += 1
+            else:
+                dictionary[i] = 1
+        return dictionary
 def main():
     limit = int(input())
     for i in range(limit):
         temp = input().split(" ")
-        dic = tokenize(temp)
+        dic = tokenize(temp,i)
     print(dic)
 if __name__ == '__main__':
     main()
