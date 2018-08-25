67542f3e1151c1ca60292227a24a863f2ed990cc
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-25 10:29:12 +0530

    " -> 2 of 2 passed. pylint: 8.57/10 "

--- CSPP1-Practice/CSPP1-Assignments/M22/assignment 1/read_input.py
+++ CSPP1-Practice/CSPP1-Assignments/M22/assignment 1/read_input.py
@@ -1,12 +1,11 @@
 '''
 Write a python program to read multiple lines of text input and store the input into a string.
 '''
-
 def main():
+    '''main method'''
     val = int(input())
     for i in range(val):
-    	ans = input()
-    	print(ans)
-
+        ans = input()
+        print(ans)
 if __name__ == '__main__':
     main()
