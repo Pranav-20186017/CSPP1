b85827b3014e1715435bbd386675b08c22eceedc
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-25 11:03:59 +0530

    " -> 3 of 3 passed. pylint: 7.14/10 "

--- CSPP1-Practice/CSPP1-Assignments/M22/assignment 3/tokenize.py
+++ CSPP1-Practice/CSPP1-Assignments/M22/assignment 3/tokenize.py
@@ -2,20 +2,19 @@
 Write a function to tokenize a given string and return a dictionary with the frequency of
 each word
 '''
-
 def tokenize(temp):
-	dictionary = dict()
-	for i in temp:
-		if i in dictionary.keys():
-			dictionary[i] += 1
-		else:
-			dictionary[i] = 1
-	return dictionary
+    dictionary = dict()
+    for i in temp:
+        if i in dictionary.keys():
+            dictionary[i] += 1
+        else:
+            dictionary[i] = 1
+    return dictionary
 def main():
     limit = int(input())
     for i in range(limit):
-    	temp = input().strip('",.;').split(" ")
-    	dic = tokenize(temp)
+        temp = input().strip('",.;').split(" ")
+        dic = tokenize(temp)
     print(dic)
 if __name__ == '__main__':
     main()
--- CSPP1-Practice/CSPP1-Assignments/M22/assignment 4/print_dictionary.py
+++ CSPP1-Practice/CSPP1-Assignments/M22/assignment 4/print_dictionary.py
@@ -5,8 +5,14 @@ the key and frequency with a SPACE - SPACE.
 '''
 
 def print_dictionary(dictionary):
-    pass
-
+    new_dict = dict()
+    keys = sorted(list(dictionary.keys()))
+    for i in keys:
+        new_dict[i] = dictionary[i]
+    keys_new = list(new_dict.keys())
+    vals_new = list(new_dict.values())
+    for i in range(len(keys_new)):
+        print(str(keys_new[i]) + " " + "-" + " " + str(vals_new[i]))
 def main():
     dictionary = eval(input())
     print_dictionary(dictionary)
