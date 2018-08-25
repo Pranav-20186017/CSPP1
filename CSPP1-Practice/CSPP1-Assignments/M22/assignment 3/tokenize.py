97289a02ea34e5e5b89c361a0c2a24012e6d8f80
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-25 11:31:56 +0530

    " -> 3 of 4 passed. pylint: 6.00/10 "

--- CSPP1-Practice/CSPP1-Assignments/M22/assignment 3/tokenize.py
+++ CSPP1-Practice/CSPP1-Assignments/M22/assignment 3/tokenize.py
@@ -3,19 +3,18 @@ Write a function to tokenize a given string and return a dictionary with the fre
 each word
 '''
 def tokenize(temp,i):
-    if i == 0:
-        new_list=[]
-        for i in range(len(temp)):
-            string = str(temp[i])
-            string = string.strip('",.;')
-            new_list.append(string)
-        dictionary = dict()
-        for i in new_list:
-            if i in dictionary.keys():
-                dictionary[i] += 1
-            else:
-                dictionary[i] = 1
-        return dictionary
+    new_list=[]
+    for i in range(len(temp)):
+        string = str(temp[i])
+        string = string.strip('",.;')
+        new_list.append(string)
+    dictionary = dict()
+    for i in new_list:
+        if i in dictionary.keys():
+            dictionary[i] += 1
+        else:
+            dictionary[i] = 1
+    return dictionary
 def main():
     limit = int(input())
     for i in range(limit):
