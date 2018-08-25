133d29cfd2647297c6ff670f3ff4ad20a16edf58
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-25 10:44:07 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- CSPP1-Practice/CSPP1-Assignments/M22/assignment 2/clean_input.py
+++ CSPP1-Practice/CSPP1-Assignments/M22/assignment 2/clean_input.py
@@ -1,17 +1,16 @@
 '''
-Write a function to clean up a given string by removing the special characters and retain 
+Write a function to clean up a given string by removing the special characters and retain
 alphabets in both upper and lower case and numbers.
 '''
-
 def clean_string(string):
     '''clean the string'''
     final_ans = ""
     for i in string:
         if ord(i) >= 65 and ord(i) <= 90:
             final_ans += i
-        if ord(i) >= 97 and ord(i) <=122:
+        if ord(i) >= 97 and ord(i) <= 122:
             final_ans += i
-        if ord(i) >=48 and ord(i) <= 57:
+        if ord(i) >= 48 and ord(i) <= 57:
             final_ans += i
     return final_ans
 def main():
