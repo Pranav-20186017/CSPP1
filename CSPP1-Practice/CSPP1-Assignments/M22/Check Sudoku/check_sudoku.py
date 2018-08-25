5f14bcd6d7de600fed003f5d58fe1fa84d68915f
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-25 11:58:10 +0530

    " -> 26 of 26 passed. pylint: 8.12/10 "

--- CSPP1-Practice/CSPP1-Assignments/M22/Check Sudoku/check_sudoku.py
+++ CSPP1-Practice/CSPP1-Assignments/M22/Check Sudoku/check_sudoku.py
@@ -16,7 +16,7 @@ def check_sudoku(sudoku):
     grid = np.array(sudoku)
     for i in range(9):
         j, k = (i // 3) * 3, (i % 3) * 3
-        if len(set(grid[i, :])) != 9 or len(set(grid[:, i])) != 9
+        if len(set(grid[i, :])) != 9 or len(set(grid[:, i])) != 9:
                    # or len(set(grid[j:j+3, k:k+3].ravel())) != 9:
             return False
     return True
