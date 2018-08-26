ca34dede7de7e99f20d53f76571bb0d6b2a591ce
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-08-26 09:56:28 +0530

    " -> 2 of 2 passed. pylint: 9.03/10 "

--- CSPP1-Practice/CSPP1-Assignments/M12/p2/social_network.py
+++ CSPP1-Practice/CSPP1-Assignments/M12/p2/social_network.py
@@ -44,7 +44,7 @@ def delete_person(network, arg1):
         update the network dictionary and return it
     '''
     for i in network.values():
-        if(arg1 in i):
+        if arg1 in i:
             i.remove(arg1)
     if arg1 in network:
         del network[arg1]
