2df89678dd9717b95ed02d7b216b8011b0f511ee
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-09-30 22:25:47 +0530

    "pylint_score -> 0 of 5 passed. style score: 0/0 "

---
+++ CSPP1-Practice/MSIT_MISC/8.1 Selection Sort/Solution.py
@@ -0,0 +1,16 @@
+def selection_sort(array):
+	b = []
+	for i in range(0, len(array)):
+		temp = min(array)
+		ind = array.index(temp)
+		b.append(temp)
+		del array[i]
+	return b
+def main():
+	n = int(input())
+	for i in range(n):
+		a = input().split(" ")
+		result = selection_sort(a)
+		print(result)
+if __name__ == "__main__":
+	main()
