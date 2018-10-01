6c584e441a6b3fa6bc89d9f424fe73a7d09af37e
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-01 08:21:28 +0530

    "pylint_score -> 5 of 5 passed. style score: 4/10 "

--- CSPP1-Practice/MSIT_MISC/8.1 Selection Sort/Solution.py
+++ CSPP1-Practice/MSIT_MISC/8.1 Selection Sort/Solution.py
@@ -1,20 +1,28 @@
+'''
+*********************************
+Author: Pranav Surampudi
+Date: 1 October 2018
+Encoding: utf-8
+**********************************
+'''
 def selection_sort(array):
-	b = []
-	st = "["
-	for i in range(0, len(array)):
-		temp = min(array)
-		ind = array.index(temp)
-		b.append(temp)
-		del array[ind]
-	for i in range(0,len(b)):
-		st = st + b[i] + ", "
-	res = st[:-2] + "]"
-	return res
+    b = []
+    st = "["
+    length = len(array)
+    for i in range(0, length):
+        temp = min(array)
+        ind = array.index(temp)
+        b.append(temp)
+        del array[ind]
+    for i in range(0,len(b)):
+        st = st + b[i] + ", "
+    res = st[:-2] + "]"
+    return res
 def main():
-	n = int(input())
-	for i in range(n):
-		a = input().split(" ")
-		result = selection_sort(a)
-		print(result)
+    n = int(input())
+    for i in range(n):
+        a = input().split(" ")
+        result = selection_sort(a)
+        print(result)
 if __name__ == "__main__":
-	main()
+    main()
